2d63226cb89c4e728311c87be2c64911e341e789
VoDinhVan <20522147@gm.uit.edu.vn>
2022-05-22 22:03:15 +0700

    Code Logic Fruit

--- background.py
+++ background.py
@@ -13,6 +13,12 @@ class FRUIT:
         self.y=random.randint(0,cell_number-1)
         self.pos=Vector2(self.x,self.y)
 
+    def draw_fruit(self):
+        fruit_rect=pygame.Rect(int(self.pos.x*cell_size),int(self.pos.y*cell_size),cell_size,cell_size)
+        screen.blit(apple,fruit_rect)
+
+  
+
 
 
 pygame.init()
@@ -21,7 +27,10 @@ cell_number=20
 
 screen= pygame.display.set_mode((cell_number*cell_size,cell_number*cell_size))
 clock =pygame.time.Clock()
+apple= pygame.image.load('Graphics/apple.png').convert_alpha()
 
+
+fruit= FRUIT()
 
 
 while True: 
@@ -31,5 +40,6 @@ while True:
             )
             sys.exit()
     screen.fill((100,215,70))
+    fruit.draw_fruit()
     pygame.display.update()
     clock.tick(60)
